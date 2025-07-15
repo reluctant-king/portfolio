e3e470288ba438436c15d1e45adff1e6e7455850
Ambady Unnikrishnan <ambadyunnikrishnan2000@gmail.com>
2025-07-15 21:38:10 +0530

    project five

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-  
+function sentMessage(event){
+  event.preventDefault();
+    const message = document.getElementById("message").value;
+    if (message.trim() === "") {
+        alert("Please enter a message before sending.");
+        return;
+    }
+    alert("Message sent: ");
+
+}
